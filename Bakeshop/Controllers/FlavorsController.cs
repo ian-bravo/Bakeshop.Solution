a1539e73648361217681353caed7a0eb6059370d
ian-bravo <bravo.ian@gmail.com>
2023-10-20 13:32:28 -0700

    add flavor index/controller

---
+++ Bakeshop/Controllers/FlavorsController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Bakeshop.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace Bakeshop.Controllers
+{
+  // [Authorize]
+  public class FlavorsController : Controller
+  {
+    private readonly BakeshopContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public FlavorsController(UserManager<ApplicationUser> userManager, BakeshopContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+    
+    public ActionResult Index()
+    {
+      return View(_db.Flavors.ToList());
+    }
+  }
+}
