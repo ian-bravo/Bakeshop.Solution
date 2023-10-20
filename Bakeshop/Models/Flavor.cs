9a7cd127935194b19a5582f5548c6dcb5ee33822
ian-bravo <bravo.ian@gmail.com>
2023-10-20 10:41:48 -0700

    refactor models

--- Bakeshop/Models/Flavor.cs
+++ Bakeshop/Models/Flavor.cs
@@ -8,6 +8,8 @@ namespace Bakeshop.Models
     public int FlavorId { get; set; }
     [Required(ErrorMessage = "Must input a type of flavor!")]
     public string Type { get; set; }
+    [Required(ErrorMessage = "Must input a description!")]
+    public string Description { get; set; }
     public List<FlavorTreat> JoinEntities { get; }
     public ApplicationUser User { get; set; } 
   }
--- Bakeshop/Models/Treat.cs
+++ Bakeshop/Models/Treat.cs
@@ -8,6 +8,8 @@ namespace Bakeshop.Models
     public int TreatId { get; set; }
     [Required(ErrorMessage = "Must input a type of treat!")]
     public string Type { get; set; }
+    [Required(ErrorMessage = "Must input a description!")]
+    public string Description { get; set; }
     public List<FlavorTreat> JoinEntities { get; }
     public ApplicationUser User { get; set; } 
   }
